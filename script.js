a5fc03096361b6d579aeb9408e7ca83d819345f2
coraldospuntos <144595105+coraldospuntos@users.noreply.github.com>
2023-09-10 16:19:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
 // Function to fetch and display data
-function fetchDataAndDisplay(url, targetElementId) {
+function fetchDataAndDisplay(url, targetTableId) {
     fetch(url)
         .then(response => response.text())
         .then(data => {
-            const parsedData = Papa.parse(data, { header: true }).data;
-            const table = createTable(parsedData);
-            document.getElementById(targetElementId).appendChild(table);
+            const rows = data.split('\n');
+            const table = document.getElementById(targetTableId);
+
+            // Create table headers
+            const thead = document.createElement('thead');
+            const headerRow = document.createElement('tr');
+
+            const headers = rows[0].split(';'); // Split the first row by semicolon
+            for (const header of headers) {
+                const th = document.createElement('th');
+                th.textContent = header;
+                headerRow.appendChild(th);
+            }
+            thead.appendChild(headerRow);
+            table.appendChild(thead);
+
+            // Create table rows
+            const tbody = document.createElement('tbody');
+            for (let i = 1; i < rows.length; i++) {
+                const row = document.createElement('tr');
+                const cells = rows[i].split(';'); // Split each row by semicolon
+                for (const cell of cells) {
+                    const td = document.createElement('td');
+                    td.textContent = cell;
+                    row.appendChild(td);
+                }
+                tbody.appendChild(row);
+            }
+            table.appendChild(tbody);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
         });
 }
 
-// Helper function to create a table from CSV data
-function createTable(data) {
-    const table = document.createElement('table');
-    const headerRow = document.createElement('tr');
+// Fetch and display Pokémon data from the "data" subfolder
+fetchDataAndDisplay('data/mon-data.csv', 'pokemon-data');
 
-    // Create table headers from the CSV column names
-    for (const column of Object.keys(data[0])) {
-        const th = document.createElement('th');
-        th.textContent = column;
-        headerRow.appendChild(th);
-    }
-
-    table.appendChild(headerRow);
-
-    // Populate table rows with data
-    for (const row of data) {
-        const dataRow = document.createElement('tr');
-
-        for (const column of Object.values(row)) {
-            const td = document.createElement('td');
-            td.textContent = column;
-            dataRow.appendChild(td);
-        }
-
-        table.appendChild(dataRow);
-    }
-
-    return table;
-}
-
-// Fetch and display Pokémon data
-fetchDataAndDisplay('mon-data.csv', 'pokemon-data');
-
-// Fetch and display Poké Ball data
-fetchDataAndDisplay('ball-data.csv', 'pokeball-data');
+// Fetch and display Poké Ball data from the "data" subfolder
+fetchDataAndDisplay('data/ball-data.csv', 'pokeball-data');
