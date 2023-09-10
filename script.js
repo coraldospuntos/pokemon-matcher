f47ac6e8cd83d80329033630a05724c67ceec3fa
coraldospuntos <144595105+coraldospuntos@users.noreply.github.com>
2023-09-10 16:03:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function fetchPokemonData() {
         .then(data => {
             const parsedData = Papa.parse(data, { header: true }).data;
             const pokemonDataContainer = document.getElementById('pokemon-data');
-            pokemonDataContainer.textContent = JSON.stringify(parsedData, null, 2);
+            createTable(pokemonDataContainer, parsedData);
         })
         .catch(error => {
             console.error('Error fetching Pokémon data:', error);
@@ -19,11 +19,43 @@ function fetchPokeballData() {
         .then(data => {
             const parsedData = Papa.parse(data, { header: true }).data;
             const pokeballDataContainer = document.getElementById('pokeball-data');
-            pokeballDataContainer.textContent = JSON.stringify(parsedData, null, 2);
+            createTable(pokeballDataContainer, parsedData);
         })
         .catch(error => {
             console.error('Error fetching Poké Ball data:', error);
         });
+}
+
+// Function to create a table from parsed CSV data
+function createTable(container, data) {
+    const table = document.createElement('table');
+    const headers = Object.keys(data[0]);
+
+    // Create table headers
+    const thead = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    headers.forEach(header => {
+        const th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Create table rows
+    const tbody = document.createElement('tbody');
+    data.forEach(rowData => {
+        const row = document.createElement('tr');
+        headers.forEach(header => {
+            const cell = document.createElement('td');
+            cell.textContent = rowData[header];
+            row.appendChild(cell);
+        });
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+
+    container.appendChild(table);
 }
 
 // Call the functions to fetch and display data
