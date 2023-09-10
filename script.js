27b6776f506bf6b3fc9a30f71cbbe17f43c41b51
coraldospuntos <144595105+coraldospuntos@users.noreply.github.com>
2023-09-10 16:12:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,49 @@
-// Fetch and display Pokémon data
-fetch('mon-data.csv')
-    .then(response => response.text())
-    .then(data => {
-        const pokemonData = Papa.parse(data, { header: true }).data;
-        const pokemonTable = createTable(pokemonData);
-        document.getElementById('pokemon-data').appendChild(pokemonTable);
-    })
-    .catch(error => {
-        console.error('Error fetching Pokémon data:', error);
-    });
-
-// Fetch and display Poké Ball data
-fetch('ball-data.csv')
-    .then(response => response.text())
-    .then(data => {
-        const pokeballData = Papa.parse(data, { header: true }).data;
-        const pokeballTable = createTable(pokeballData);
-        document.getElementById('pokeball-data').appendChild(pokeballTable);
-    })
-    .catch(error => {
-        console.error('Error fetching Poké Ball data:', error);
-    });
+// Function to fetch and display data
+function fetchDataAndDisplay(url, targetElementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const parsedData = Papa.parse(data, { header: true }).data;
+            const table = createTable(parsedData);
+            document.getElementById(targetElementId).appendChild(table);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
 
 // Helper function to create a table from CSV data
 function createTable(data) {
     const table = document.createElement('table');
     const headerRow = document.createElement('tr');
-    
+
     // Create table headers from the CSV column names
     for (const column of Object.keys(data[0])) {
         const th = document.createElement('th');
         th.textContent = column;
         headerRow.appendChild(th);
     }
-    
+
     table.appendChild(headerRow);
 
     // Populate table rows with data
     for (const row of data) {
         const dataRow = document.createElement('tr');
-        
+
         for (const column of Object.values(row)) {
             const td = document.createElement('td');
             td.textContent = column;
             dataRow.appendChild(td);
         }
-        
+
         table.appendChild(dataRow);
     }
 
     return table;
 }
+
+// Fetch and display Pokémon data
+fetchDataAndDisplay('mon-data.csv', 'pokemon-data');
+
+// Fetch and display Poké Ball data
+fetchDataAndDisplay('ball-data.csv', 'pokeball-data');
