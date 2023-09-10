00de2ae59481b384476b5e2fe844c29c0ded2276
coraldospuntos <144595105+coraldospuntos@users.noreply.github.com>
2023-09-10 16:00:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-// Function to fetch and display CSV data
-async function displayCSVData() {
-    const response = await fetch('data/mon-data.csv');
-    const data = await response.text();
-
-    const rows = data.split('\n');
-    const headers = rows[0].split(',');
-
-    const table = document.getElementById('pokemon-table');
-
-    // Populate table headers
-    const thead = table.querySelector('thead tr');
-    headers.forEach(header => {
-        const th = document.createElement('th');
-        th.textContent = header;
-        thead.appendChild(th);
-    });
-
-    // Populate table data
-    const tbody = table.querySelector('tbody');
-    for (let i = 1; i < rows.length; i++) {
-        const cells = rows[i].split(',');
-        const tr = document.createElement('tr');
-
-        cells.forEach(cell => {
-            const td = document.createElement('td');
-            td.textContent = cell;
-            tr.appendChild(td);
+// Function to fetch and display Pokémon data
+function fetchPokemonData() {
+    fetch('data/mon-data.csv') // Adjust the path to your CSV file
+        .then(response => response.text())
+        .then(data => {
+            const parsedData = Papa.parse(data, { header: true }).data;
+            const pokemonDataContainer = document.getElementById('pokemon-data');
+            pokemonDataContainer.textContent = JSON.stringify(parsedData, null, 2);
+        })
+        .catch(error => {
+            console.error('Error fetching Pokémon data:', error);
         });
-
-        tbody.appendChild(tr);
-    }
 }
 
-// Call the function to display CSV data when the page loads
-displayCSVData();
+// Function to fetch and display Poké Ball data
+function fetchPokeballData() {
+    fetch('data/ball-data.csv') // Adjust the path to your CSV file
+        .then(response => response.text())
+        .then(data => {
+            const parsedData = Papa.parse(data, { header: true }).data;
+            const pokeballDataContainer = document.getElementById('pokeball-data');
+            pokeballDataContainer.textContent = JSON.stringify(parsedData, null, 2);
+        })
+        .catch(error => {
+            console.error('Error fetching Poké Ball data:', error);
+        });
+}
+
+// Call the functions to fetch and display data
+fetchPokemonData();
+fetchPokeballData();
